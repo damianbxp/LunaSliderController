846dca994d6fc8d7b99e0ad40680d8055bdc3eb9
damianbxp <damianb.xp@gmail.com>
2022-02-15 18:36:14 +0100

    Added look_at function

--- ScaraController.py
+++ ScaraController.py
@@ -4,6 +4,8 @@ from Vector import *
 
 class Scara:
     def __init__(self):
+        self.current_pos = Vector2(0, 0)
+
         self.a1 = 0
         self.a2 = 0
 
@@ -12,7 +14,8 @@ class Scara:
     def calc_Scara_IK(self, pos: Vector2):
         theta2 = math.acos((pos.x ** 2 + pos.y ** 2 - self.a1 ** 2 - self.a2 ** 2) / (2 * self.a1 * self.a2))
         theta1 = math.atan(pos.y / pos.x) - math.atan(
-            (self.a2 * math.sin(theta2)) / (self.a1 + self.a2 * math.cos(theta2)))
+            (self.a2 * math.sin(theta2))
+            / (self.a1 + self.a2 * math.cos(theta2)))
 
         theta1 = math.degrees(theta1)
         theta2 = math.degrees(theta2)
@@ -20,3 +23,7 @@ class Scara:
         print(f"SCARA Angles: {theta1}° {theta2}°")
         return {theta1, theta2}
 
+    def look_at(self, target:Vector2):
+        look_at_angle = math.degrees(math.atan2(target.y - self.current_pos.y, target.x - self.current_pos.x))
+        print(f"Look At angle: {look_at_angle}°")
+        return look_at_angle
